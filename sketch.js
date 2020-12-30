2c53181cf69e723b34b581d6f910e3f857def94d
cthunternhu <74863180+cthunternhu@users.noreply.github.com>
2020-12-30 17:22:36 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/BlP1OZ89F/';
+let imageModelURL = 'https://cthunternhu.github.io/maskjs/';
 
 // Video
 let video;
